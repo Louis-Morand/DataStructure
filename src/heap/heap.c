80848e70939ddb2112d817b6856b19698fab5383
benoit-arquilliere <benoit.arquilliere@gmail.com>
2021-10-15 12:57:58 +0200

    Update: Heap

--- src/heap/heap.c
+++ src/heap/heap.c
@@ -32,7 +32,7 @@ void init_heap(Heap *h)
  */
 bool is_heap_empty(Heap *h)
 {
-    bool isEmpty = h->index == -1;
+    bool isEmpty = h->index == 0;
     return isEmpty;
 }
 
@@ -44,8 +44,14 @@ bool is_heap_empty(Heap *h)
  */
 float pop_heap(Heap *h)
 {
-    float val;
-    //TODO: supprimer la valeur root
+    float val = h->data[0];
+
+    for (int i = 0; i < (h->index); i++)
+    {
+        h->data[i] = h->data[i + 1];
+    }
+    h->index--;
+    
     return val;
 }
 
@@ -57,13 +63,13 @@ float pop_heap(Heap *h)
  */
 void push_heap(Heap *h, float value)
 {
-    // h->index++;
-    // for (int i = h->index; i > 0; i--)
-    // {
-    //     h->data[i] = h->data[i-1];
-    // }
-    //
-    // h->data[0] = value;
+    h->index++;
+    for (int i = h->index; i > 0; i--)
+    {
+        h->data[i] = h->data[i-1];
+    }
+    
+    h->data[0] = value;
 }
 
 /**
@@ -101,12 +107,73 @@ void clear_heap(Heap *h)
     h->index = 0;
 }
 
-// //En option
-// void heapify(Heap *s, float array[], size_t array_size)
-// {
 
-// } //create Heap from array
-// void merge(Heap *dest, Heap *src)
+// /**
+//  * @brief 
+//  * 
+//  * @param a 
+//  * @param b 
+//  */
+// void swap(float *a, float *b)
 // {
+//     float temp = *a;
+//     *a = *b;
+//     *b = temp;
+// }
 
-// } //merge src into dest
+// /**
+//  * @brief tri les valeurs par avec le tri par tas
+//  * 
+//  * @param tab tableau float a trier
+//  * @param size taille du tableau
+//  * @param i 
+//  * @param sens_tri croissant ou décroissant
+//  */
+// void heapify(float *tab, int size, int i, int sens_tri)
+// {
+//     int largest = i;
+//     int left = 2 * i + 1;
+//     int right = 2 * i + 2;
+
+//     if (left < size && (tab[left] > tab[largest]) ^ !sens_tri) //opérateur XOR ^, 1+1 =1
+//         largest = left;
+
+//     if (right < size && (tab[right] > tab[largest]) ^ !sens_tri)
+//         largest = right;
+
+//     if (largest != i)
+//     {
+//         swap(&tab[i], &tab[largest]);
+//         heapify(tab, size, largest, sens_tri);
+//     }
+// }
+
+// /**
+//  * @brief tri les valeurs par avec le tri par tas
+//  * 
+//  * @param tab tableau float a trier
+//  * @param size taille du tableau
+//  * @param sens_tri croissant ou décroissant
+//  */
+// void heapsort(float *tab, int size, int sens_tri)
+// {
+//     for (int i = size / 2 - 1; i >= 0; i--)
+//     {
+//         heapify(tab, size, i, sens_tri);
+//     }
+//     for (int i = size - 1; i >= 0; i--)
+//     {
+//         swap(&tab[0], &tab[i]);
+//         heapify(tab, i, 0, sens_tri);
+//     }
+// }
+
+// // //En option
+// // void heapify(Heap *s, float array[], size_t array_size)
+// // {
+
+// // } //create Heap from array
+// // void merge(Heap *dest, Heap *src)
+// // {
+
+// // } //merge src into dest
