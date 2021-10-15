af7716ac5b111c4eec00321e17680697b99ddb54
benoit-arquilliere <benoit.arquilliere@gmail.com>
2021-10-15 12:45:10 +0200

    Update: heap

--- src/heap/heap.c
+++ src/heap/heap.c
@@ -13,62 +13,6 @@
 #include <stdbool.h>
 #include "heap.h"
 
-
-
-void swap(float *a, float *b)
-{
-    float temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-/**
- * @brief tri les valeurs par avec le tri par tas
- * 
- * @param tab tableau float a trier
- * @param size taille du tableau
- * @param i 
- * @param sens_tri croissant ou décroissant
- */
-void heapify(float *tab, int size, int i, int sens_tri)
-{
-    int largest = i;
-    int left = 2 * i + 1;
-    int right = 2 * i + 2;
-
-    if (left < size && (tab[left] > tab[largest]) ^ !sens_tri)//opérateur XOR ^, 1+1 =1 
-        largest = left;
-
-    if (right < size && (tab[right] > tab[largest]) ^ !sens_tri)
-        largest = right;
-
-    if (largest != i)
-    {
-        swap(&tab[i], &tab[largest]);
-        heapify(tab, size, largest, sens_tri);
-    }
-}
-
-/**
- * @brief tri les valeurs par avec le tri par tas
- * 
- * @param tab tableau float a trier
- * @param size taille du tableau
- * @param sens_tri croissant ou décroissant
- */
-void heapsort(float *tab, int size, int sens_tri)
-{
-    for (int i = size / 2 - 1; i >= 0; i--)
-    {
-        heapify(tab, size, i, sens_tri);
-    }
-    for (int i = size - 1; i >= 0; i--)
-    {
-        swap(&tab[0], &tab[i]);
-        heapify(tab, i, 0, sens_tri);
-    }
-}
-
 /**
  * @brief
  *
@@ -76,7 +20,7 @@ void heapsort(float *tab, int size, int sens_tri)
  */
 void init_heap(Heap *h)
 {
-    h->index = -1;
+    h->index = 0;
 }
 
 /**
@@ -101,6 +45,7 @@ bool is_heap_empty(Heap *h)
 float pop_heap(Heap *h)
 {
     float val;
+    //TODO: supprimer la valeur root
     return val;
 }
 
@@ -112,8 +57,13 @@ float pop_heap(Heap *h)
  */
 void push_heap(Heap *h, float value)
 {
-    h->index++;
-    // TODO: add a value to heap
+    // h->index++;
+    // for (int i = h->index; i > 0; i--)
+    // {
+    //     h->data[i] = h->data[i-1];
+    // }
+    //
+    // h->data[0] = value;
 }
 
 /**
@@ -125,9 +75,9 @@ void push_heap(Heap *h, float value)
  */
 float replace(Heap *h, float value)
 {
-    float val;
-    // TODO: pop root and push a new key.
-    return val;
+    pop_heap(h);
+    push_heap(h, value);
+    return value;
 }
 
 /**
@@ -148,7 +98,7 @@ float peek_heap(Heap *h)
  */
 void clear_heap(Heap *h)
 {
-    h->index = -1;
+    h->index = 0;
 }
 
 // //En option
