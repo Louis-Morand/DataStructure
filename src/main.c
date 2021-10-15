8a9487010c967843ed89c25f17cb21f7b5b82d7a
opystoglyphe <louis.morand8.5@gmail.com>
2021-10-15 12:30:45 +0200

    NEw: tests avec CUnit

--- src/main.c
+++ src/main.c
@@ -24,111 +24,119 @@
 
 /**
  * @mainpage Datastructure Documentation
- * 
+ *
  * @brief Documentation du projet DataStructure, qui vise à implémenter et manipuler des structures de données
- * 
+ *
  * Le but de ce Tp est d'implémenter quatre types de structures de données:
  * Une pile, une file, une liste, et un tas.
- * 
+ *
  * Et d'exercer différentes manipulations de leurs données, telle que l'ajout, l'initialisation,
  * l'ajout en début ou fin de liste, et le remplacement de valeurs.
- * 
+ *
  * Actuellement, pour des mesures de simplicité, les types de tris sont implémentés via des tableaux,
  * Une modification future pourrait etre de passer via la véritable forme de leurs implémentations, ou
  * pour cette implémentation, de vérifier, en cas d'ajout de nouvelle valeur, si le tableau n'est pas deja plein.
- * 
- * 
+ *
+ *
  */
-/*
-TODO: TEST_CLEANUP ET TEST_INIT
-CU_INITIALIZE_REGISTERY POUR INIT
-CU_pSuite *p = CU_add_suite("test", test_init, test_cleanup);
-CU_add_test(p,"NOMTEST", NOMTESTFONCTION);
-CU_basic_run_tests(); pour run
 
-*/
-
-void test_array_list(Array_list *liste)
+void test_array_list()
 {
-    init_array_list(liste);
-    insert_at(liste, 2, 56);
-    assert(liste->data[1] == 56);
-    add(liste, 99);
-    assert(liste->data[2] == 99);
-    remove_at(liste, 2);
-    assert(liste->data[1] == 99);
-    assert((get_at(liste, 2)) == 99);
-    clear_array(liste);
-    assert(liste->index == 0);
+    Array_list *array = (Array_list *)(malloc(sizeof(Array_list)));
+
+    init_array_list(array);
+    insert_at(array, 2, 56);
+    CU_ASSERT(array->data[1] == 56);
+    add(array, 99);
+    CU_ASSERT(array->data[2] == 99);
+    remove_at(array, 2);
+    CU_ASSERT(array->data[1] == 99);
+    CU_ASSERT((get_at(array, 2)) == 99);
+    clear_array(array);
+    CU_ASSERT(array->index == 0);
 }
-void test_queue(Queue *queue)
+void test_queue()
 {
+    Queue *queue = (Queue *)malloc(sizeof(Queue));
+
     init_queue(queue);
-    assert(is_queue_empty(queue) == true);
+    CU_ASSERT(is_queue_empty(queue) == true);
     enqueue(queue, 5);
-    assert((queue->data[queue->index - 1]) == 5);
-    assert((front(queue)) == 5);
+    CU_ASSERT((queue->data[queue->index - 1]) == 5);
+    CU_ASSERT((front(queue)) == 5);
     enqueue(queue, 6);
-    assert(queue->data[queue->index - 1] == 6);
+    CU_ASSERT(queue->data[queue->index - 1] == 6);
     enqueue(queue, 990);
-    assert(queue->data[queue->index - 1] == 990);
-    assert((dequeue(queue)) == 5);
-    assert(front(queue) == 6);
-    assert(is_queue_empty(queue) == false);
+    CU_ASSERT(queue->data[queue->index - 1] == 990);
+    CU_ASSERT((dequeue(queue)) == 5);
+    CU_ASSERT(front(queue) == 6);
+    CU_ASSERT(is_queue_empty(queue) == false);
     clear_queue(queue);
-    assert(is_queue_empty(queue) == true);
+    CU_ASSERT(is_queue_empty(queue) == true);
 }
 
-void test_stack(Stack *stack)
+void test_stack()
 {
+    Stack *stack = (Stack *)malloc(sizeof(Stack));
 
     init_stack(stack);
-    assert(is_stack_empty(stack) == true);
+    CU_ASSERT(is_stack_empty(stack) == true);
     push_stack(stack, 18);
-    assert(stack->data[stack->index]);
+    CU_ASSERT(stack->data[stack->index]);
     push_stack(stack, 4);
-    assert(stack->data[stack->index] == 4);
+    CU_ASSERT(stack->data[stack->index] == 4);
     push_stack(stack, 63);
-    assert(stack->data[stack->index] == 63);
-    assert(peek_stack(stack) == 63);
-    assert(pop_stack(stack) == 63);            // on enleve le 63, reste 18 et 4
+    CU_ASSERT(stack->data[stack->index] == 63);
+    CU_ASSERT(peek_stack(stack) == 63);
+    CU_ASSERT(pop_stack(stack) == 63);            // on enleve le 63, reste 18 et 4
     swap_stack(stack);                         // on echange le 18 et le 4
-    assert((stack->data[stack->index]) == 18); // donc 18 est en position 2
-    assert((stack->data[stack->index - 1]) == 4);
+    CU_ASSERT((stack->data[stack->index]) == 18); // donc 18 est en position 2
+    CU_ASSERT((stack->data[stack->index - 1]) == 4);
     dup_stack(stack);
-    assert(stack->data[stack->index] == stack->data[stack->index - 1]);
+    CU_ASSERT(stack->data[stack->index] == stack->data[stack->index - 1]);
 }
 
-void test_heap(Heap *heap)
+void test_heap()
 {
+    Heap *heap = (Heap *)(malloc(sizeof(Heap)));
+
     init_heap(heap);
-    assert(is_heap_empty(heap) == true);
-    push_heap(heap,55);
-    assert(peek_heap(heap)== 55);
+    CU_ASSERT(is_heap_empty(heap) == true);
+    push_heap(heap, 55);
+    CU_ASSERT(peek_heap(heap) == 55);
     push_heap(heap, 66);
-    assert(heap->data[2]==66);
-    push_heap(heap,89);
-    assert(heap->data[2]==89);
-    assert(pop_heap(heap)==55); // pop the root value
-    assert(replace(heap,10)==66);// pop root and push a new key.
-    assert(peek_heap(heap)==10);
+    CU_ASSERT(heap->data[2] == 66);
+    push_heap(heap, 89);
+    CU_ASSERT(heap->data[2] == 89);
+    CU_ASSERT(pop_heap(heap) == 55);    // pop the root value
+    CU_ASSERT(replace(heap, 10) == 66); // pop root and push a new key.
+    CU_ASSERT(peek_heap(heap) == 10);
     void clear_heap(Heap * h);
-    assert(is_heap_empty(heap) == true);
+    CU_ASSERT(is_heap_empty(heap) == true);
+}
+
+int test_init()
+{
+    return 0;
+}
+int test_cleanup()
+{
+    return 0;
 }
 
 
 int main(int argc, char **argv)
 {
-    Queue *queue = (Queue *)malloc(sizeof(Queue));
-    Stack *stack = (Stack *)malloc(sizeof(Stack));
-    Array_list *array = (Array_list *)(malloc(sizeof(Array_list)));
-    Heap *heap = (Heap *)(malloc(sizeof(Heap)));
+    CU_initialize_registry();
 
-    test_array_list(array);
-    test_queue(queue);
-    test_stack(stack);
-    // test_heap(heap);
+    CU_pSuite *tests = CU_add_suite("tests DataStructure", test_init, test_cleanup);
+
+    CU_add_test(tests, "test arrays_list", test_array_list);
+    CU_add_test(tests, "test queue", test_queue);
+    CU_add_test(tests, "test stack", test_stack);
+    // CU_add_test(tests, "test heap", test_heap);
     
-
+    CU_basic_run_tests();
+    test_cleanup();
     return (EXIT_SUCCESS);
 }
