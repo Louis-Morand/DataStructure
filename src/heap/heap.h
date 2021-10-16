c43d7cf6f1f6d7b4fbcf474f1c3020ef0ebefdb0
benoit-arquilliere <78789210+benoit-arquilliere@users.noreply.github.com>
2021-10-16 15:36:51 +0200

    Update heap.h

--- src/heap/heap.h
+++ src/heap/heap.h
@@ -25,6 +25,7 @@ typedef struct {
 
 #endif /* HEAP_H */
 
+//main functions
 void init_heap(Heap *h);
 bool is_heap_empty(Heap *h);
 float pop_heap(Heap *h); //pop the root value
@@ -33,38 +34,9 @@ float replace(Heap *h, float value); // pop root and push a new key.
 float peek_heap(Heap *h); // return root value but dont remove it
 void clear_heap(Heap *h);
 
+//utils functions
 void swap(float *a, float *b);
-
-/**
- * @brief tri les valeurs par avec le tri par tas
- * 
- * @param tab tableau float a trier
- * @param size taille du tableau
- * @param i 
- * @param sens_tri croissant ou décroissant
- */
-
 void heapify(float *tab, int size, int i);
-
-/**
- * @brief tri les valeurs par avec le tri par tas
- * 
- * @param tab tableau float a trier
- * @param size taille du tableau
- * @param sens_tri croissant ou décroissant
- */
 void heapsort(float *tab, int size);
-
-/**
- * @brief appel de la fonction test
- * 
- * @param test_number nombre de fois que le test va être réalisé
- * @param heap_results tableau contenant les résultats
- */
-void heap_sorting_test(int test_number, float *heap_results);
-
-//En option
-//void heapify(Heap *s, float array[], size_t array_size); //create Heap from array
-//void merge(Heap *dest, Heap *src); //merge src into dest
 
 
